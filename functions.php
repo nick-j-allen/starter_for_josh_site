ded49273899add9cfeaed78332baffb05b009d38
nick-j-allen <nickjallen1999@gmail.com>
2023-04-06 14:03:16 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,3 +12,14 @@ wp_enqueue_style( 'bootstrap_css', 'https://cdnjs.cloudflare.com/ajax/libs/twitt
 }
 add_action( 'wp_enqueue_scripts', 'load_my_scripts');
 ?>
+<?php
+function register_navwalker(){
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+?>
+<?php
+register_nav_menus( array(
+ 'primary' => __( 'Primary Menu', 'top-menu' ),
+) );
+?>
